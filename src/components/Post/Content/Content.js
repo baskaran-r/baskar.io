715c74d8f6a8e1a992e9625a2ecfca15a4277126
Baskaran R <gowthamforever.r@gmail.com>
2020-12-22 17:14:24 +0800

    Added title highlighting style

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -12,6 +12,7 @@ type Props = {
 const Content = ({ body, title, date }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
+    <div className={styles['content__highlight-first-char']}>{title.charAt(0).toUpperCase()}</div>
     <Meta date={date} />
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
   </div>
